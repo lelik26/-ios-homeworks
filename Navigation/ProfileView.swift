d50315080ecdf1a477c4f69d4a575d247b7d2b4d
lelik26 <lelik.van-23@yandex.ru>
2022-02-20 19:29:21 +0800

    Внесены изменения в проект

---
+++ Navigation/ProfileView.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Alex Alex on 16.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var fotoImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var textText: UITextView!
+    
+    
+    override init(frame: CGRect) {
+            super.init(frame: frame)
+            self.setupView()
+        }
+        
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            self.setupView()
+        }
+        
+        private func setupView() {
+            let view = self.loadViewFromXib()
+            self.addSubview(view)
+            view.frame = self.bounds // здесь view, загруженная из xib-файла, добавляется на вьюху класса. Ее размер соответствует размеру вьюхе класса и растягивается так же, как и сама вьюха класса.
+            view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        }
+        
+        private func loadViewFromXib() -> UIView {
+            guard let view = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView else { return UIView() }
+            
+            return view
+        }
+
+               
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Alex Alex on 16.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+  
+    @IBOutlet weak var profileView: ProfileView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        
+        }
+    }
+    
+    
+
+
